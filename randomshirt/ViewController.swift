faa82a3a764e45df0176c825d96bc6478149bf8c
Morgansimmons25 <68120537+Morgansimmons25@users.noreply.github.com>
2021-07-15 17:40:10 -0700

    random shirt

--- randomshirt/ViewController.swift
+++ randomshirt/ViewController.swift
@@ -9,11 +9,23 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    var shirtArray = ["shirt1","shirt2", "shirt3"]
+var randomshirtNumber = 0
+    
+    @IBOutlet weak var shirtimageView: UIImageView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        newshirtImage()
         // Do any additional setup after loading the view.
     }
 
-
+    @IBAction func shirtbuttonPress(_ sender: Any) {
+    }
+    func newshirtImage() {
+        randomshirtNumber = Int.random(in: 0...2)
+        
+        shirtimageView.image = UIImage(named: shirtArray[randomshirtNumber])
+    }
 }
 
